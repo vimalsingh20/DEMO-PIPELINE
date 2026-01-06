896a32fffa6253240414327b86bda1b4fb4cd86c
vimal <your-email@gmail.com>
2026-01-06 18:09:10 +0530

    all file added

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+setup(name="census-sensor",
+      version="0.0.1",
+      author = "Vimal",
+      author_email = "vimalsingh99350@gamil.com",
+      packages = find_packages(),
+      install_requires =['pandas','numpy','flask']
+      )
